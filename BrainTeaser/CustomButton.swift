6f730956fcc2230af180c600583975082d0e3c52
NIPHERY <robin.somlette@gmail.com>
2016-03-26 11:53:11 -0400

    first commit

---
+++ BrainTeaser/CustomButton.swift
@@ -0,0 +1,9 @@
+//
+//  CustomButton.swift
+//  BrainTeaser
+//
+//  Created by Robin Somlette on 26-03-2016.
+//  Copyright © 2016 Robin Somlette. All rights reserved.
+//
+
+import Foundation
---
+++ BrainTeaser/CustomText.swift
@@ -0,0 +1,9 @@
+//
+//  CustomText.swift
+//  BrainTeaser
+//
+//  Created by Robin Somlette on 26-03-2016.
+//  Copyright © 2016 Robin Somlette. All rights reserved.
+//
+
+import Foundation
